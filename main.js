035161ca04accf290786e5db0431ca2aca907eac
Tigana Reymansyah <tiganareymansyah2502@gmail.com>
2023-10-17 20:57:53 +0700

    quiziz

---
+++ main.js
@@ -0,0 +1,4 @@
+import { tambah, kali, Phi } from "./mathOperation.js";
+console.log(tambah(9,8));
+console.log(kali(8,7));
+console.log(Phi(3.14159));
---
+++ mathOperation.js
@@ -0,0 +1,10 @@
+export function tambah (x,y){
+    return x+y;
+};
+export function kali (a,b){
+    return a*b;
+};
+
+export function Phi (a){
+    return a;
+};
